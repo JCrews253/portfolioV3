473a894db12f244e1fd6a2f607ec298d5ba68ae4
James Crews <jcrews206@gmail.com>
2024-04-30 18:53:04 -0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "retro-purple": "#1E1449",
+        "retro-pink": "#CF11DD",
+      },
+    },
+  },
+  plugins: [],
+};
